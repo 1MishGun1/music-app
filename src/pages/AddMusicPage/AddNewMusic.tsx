c16a2219de48c3d5f86da425f346d1f4a6f274b8
1MishGun1 <mikhail.kudrov@gmail.com>
2024-11-27 21:22:20 +0300

    add logic add music

--- src/pages/AddMusicPage/AddNewMusic.tsx
+++ src/pages/AddMusicPage/AddNewMusic.tsx
@@ -1,3 +1,44 @@
+import React, { useState } from "react";
+import supaBase from "../../api/supabaseClient";
+
 export const AddNewMusic = () => {
-  return <div>AddNewMusic</div>;
+  const [file, setFile] = useState<File | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const selectedFile = files[0];
+      setFile(selectedFile);
+    }
+  };
+
+  const handleFormSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (file) {
+      const { error } = await supaBase.storage
+        .from("musics")
+        .upload(`/${file.name}`, file);
+      if (error) {
+        setErrorMessage(`Ошибка при загрузке файла: ${error.message}`);
+      } else {
+        setSuccessMessage("Файл запушился успешно!");
+        setErrorMessage("");
+        setFile(null);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Запушить файл</button>
+      </form>
+      {successMessage && <p>{successMessage}</p>}
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
 };
