7a01a7cd33057f3ed334a87fdbb9fb1bfe92b2cc
1MishGun1 <mikhail.kudrov@gmail.com>
2024-11-27 21:22:53 +0300

    add logic view all  music

--- src/pages/ListMusicPage/ListMusicPage.tsx
+++ src/pages/ListMusicPage/ListMusicPage.tsx
@@ -1,3 +1,67 @@
+import { useState, useEffect } from "react";
+import supaBase from "../../api/supabaseClient";
+import { BASE_URL } from "../../api/supabaseClient";
+import { FaPlay, FaPause } from "react-icons/fa";
+
 export const ListMusicPage = () => {
-  return <div>ListMusicPage</div>;
+  const [musicList, setMusicList] = useState<string[]>([]);
+  const [isPlaying, setIsPlaying] = useState<boolean>(true);
+  const [currentAudio, setCurrentAudio] = useState<string | null>(null);
+  const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(
+    null
+  );
+
+  useEffect(() => {
+    async function fetchFilesFromSupabase() {
+      const { data, error } = await supaBase.storage.from("musics").list();
+
+      if (error) {
+        console.error("Error fetching files:", error.message);
+      } else {
+        const files = data?.map((file) => file.name) || [];
+        setMusicList(files);
+      }
+    }
+
+    fetchFilesFromSupabase();
+  }, []);
+
+  const handlePlay = (fileName: string) => {
+    if (isPlaying && currentAudio === fileName) {
+      // Если аудио уже играет, останавливаем его
+      if (audioElement) {
+        audioElement.pause();
+        setIsPlaying(false);
+      }
+    } else {
+      // Если аудио не играет, начинаем воспроизведение
+      const newAudio = new Audio(
+        `${BASE_URL}/storage/v1/object/musics/${fileName}`
+      );
+      newAudio.play();
+      setCurrentAudio(fileName);
+      setIsPlaying(true);
+      setAudioElement(newAudio);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Все песенки</h1>
+      <div className="music__items">
+        {musicList.map((fileName, index) => (
+          <div key={index} className="music__item">
+            {fileName}
+            <button onClick={() => handlePlay(fileName)}>
+              {isPlaying && currentAudio === fileName ? (
+                <FaPause />
+              ) : (
+                <FaPlay />
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
