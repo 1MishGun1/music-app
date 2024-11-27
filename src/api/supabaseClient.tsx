b466d0660875215693248c5837d6f33a1a1325eb
1MishGun1 <mikhail.kudrov@gmail.com>
2024-11-27 21:21:25 +0300

    add export consts

--- src/api/supabaseClient.tsx
+++ src/api/supabaseClient.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const BASE_URL = "https://sceatxqiiglepxdhbcaj.supabase.co";
-const BASE_KEY =
+export const BASE_URL = "https://sceatxqiiglepxdhbcaj.supabase.co";
+export const BASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjZWF0eHFpaWdsZXB4ZGhiY2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwOTc1OTIsImV4cCI6MjA0NzY3MzU5Mn0.a-7pY2EU4RelA15pl4sR4wfMbqUKLbX4yQBLjixTBbM";
 
 const supaBase = createClient(BASE_URL, BASE_KEY);
